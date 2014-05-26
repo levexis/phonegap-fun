8a0d06b5663d15759340b0e766c60a89c9bdefee
Paul Cook <paul.cook@adtechzen.com>
2014-05-26 16:52:13 +0100

    video playing

---
+++ www/js/main.js
@@ -0,0 +1,42 @@
+/* this the main.js for require for our app
+ * we disable loading of this in our karma version
+ * and create a special one in test/main.js
+ */
+require.config({
+    paths: {
+        'jquery' : 'vendor/jquery-1.10.1.min',
+        'underscore' : 'vendor/underscore',
+        'backbone' : 'vendor/backbone',
+        'text' : 'vendor/text',
+        'video' : 'vendor/video'
+    },
+    shim: {
+        underscore: {
+            exports: '_'
+        },
+        jquery: {
+            exports: '$'
+        },
+        backbone: {
+            deps: ['underscore', 'jquery' ],
+            exports: 'Backbone'
+        },
+        dualStorage: {
+            deps: ['backbone'],
+            exports: 'Backbone'
+        }
+    }
+});
+
+require.onError = function (err) {
+    console.trace('requireJS error', err);
+};
+
+// bootstrap app
+require([
+    'app'
+], function(app){
+    //We expose the app for testing and debugging
+    //It can be removed on production
+    window.app = app;
+});
