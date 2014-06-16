0cbbdb4dc5e4f892b9a19ff1b9af8c3887a6c5f1
Paul Cook <paul.cook@adtechzen.com>
2014-06-16 19:01:23 +0100

    experiment with onsenui

--- angular/app/scripts/app.js
+++ angular/app/scripts/app.js
@@ -1,75 +1,33 @@
 'use strict';
+// Make sure to include the `ui.router` module as a dependency
+var myApp = angular.module('phonegapFunApp', ['ui.router', 'angularCordovaWrapper', 'onsen.directives' , 'ngAnimate'])
+    .run(
+    [        '$rootScope', '$state', '$stateParams',
+        function ($rootScope,   $state,   $stateParams) {
+            // It's very handy to add references to $state and $stateParams to the $rootScope
+            // so that you can access them from any scope within your applications.For example,
+            // <li ui-sref-active="active }"> will set the <li> // to active whenever
+            // 'contacts.list' or one of its decendents is active.
+            $rootScope.$state = $state;
+            $rootScope.$stateParams = $stateParams;
+        }]);
 
-angular.module('phonegapFunApp', [ 'ngRoute', 'angularCordovaWrapper' ,  'kendo.directives'])
-  .config(function ($routeProvider , $locationProvider) {
-      $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/organizations', {
-          templateUrl: 'views/organizations.html',
-          controller: 'OrgsCtrl'
-      })
-      .when('/main', {
-              templateUrl: 'views/main.html',
-              controller: 'MainCtrl'
-          })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+myApp.config( [ '$stateProvider', '$urlRouterProvider' , function (stateRouter , urlRouter) {
+     // For any unmatched url, redirect to /state1
+     urlRouter.otherwise("/");
+     //
+     // Now set up the states
+     stateRouter
+     .state('home', {
+         url: "/",
+         templateUrl: 'views/main.html',
+         controller: 'MainCtrl'
+     })
+     .state('orgs', {
+         url : "/organizations",
+         templateUrl : 'views/organizations.html',
+         controller : 'OrgsCtrl'
+     });
+}]);
 
-function OrgsCtrl($scope, $location ) {
-    $scope.list = [
-        "Determine project scope",
-        "Define preliminary resources",
-        "Conduct needs analysis",
-        "Draft preliminary specifications",
-        "Develop delivery timeline",
-        "Obtain approvals to proceed",
-        "Secure reuired resources",
-        "Develop functional specifications",
-        "Develop prototype",
-        "Review functional specifications",
-        "Identify design parameters",
-        "Assign development staff"
-    ];
-    $scope.collection = [
-        { id:1 , name: "Determine project scope", description: 'descript', text:'text' },
-        {id:2 , name: "Define preliminary resources"},
-        {    id:3 , name: "Conduct needs analysis"},
-        {   id:4 , name:  "Draft preliminary specifications"},
-        {   id:5 , name: "Develop delivery timeline"},
-        {   id:6 , name:  "Obtain approvals to proceed"},
-        {   id:7 , name: "Secure reuired resources"},
-        {   id:8 , name: "Develop functional specifications"},
-        {   id:9 , name: "Develop prototype"},
-        {           id:10 , name: "Review functional specifications"},
-        {   id:11 , name: "Identify design parameters"},
-        {   id:12 , name: "Assign development staff"}
-    ];
-    $scope.dataset = new kendo.data.DataSource({ data: $scope.collection });
 
-    $scope.clicker=function( what){
-        console.log( what, $scope );
-    };
-    $scope.doRoute=function( what ) {
-        console.log( what, $scope );
-        $location.path (what ).replace();
-    };
-    console.log ( 'Orgs initialised');
-}
-
-// this will get screwed up by uglify need to use
-//someModule.controller('MyController', ['$scope', 'dep1', 'dep2', function($scope,
-// doesn't seem to work at all!
-function MainCtrl( $scope, $location ) {
-    $scope.doRoute=function( what ) {
-        $location.path = what;
-    };
-    $scope.listViewClick=function( what ) {
-        console.log('clicked',what);
-    };
-    console.log ( 'Main initialised');
-}
--- angular/app/scripts/controllers/main.js
+++ angular/app/scripts/controllers/main.js
@@ -1,10 +1,53 @@
 'use strict';
 
 angular.module('phonegapFunApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', [ '$scope', '$location', function ($scope , $location) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  });
+    console.log ( 'Main initialised',$scope,$location);
+  } ]);
+
+angular.module('phonegapFunApp')
+    .controller('OrgsCtrl', [ '$scope', function ($scope) {
+    $scope.list = [
+        "Determine project scope",
+        "Define preliminary resources",
+        "Conduct needs analysis",
+        "Draft preliminary specifications",
+        "Develop delivery timeline",
+        "Obtain approvals to proceed",
+        "Secure reuired resources",
+        "Develop functional specifications",
+        "Develop prototype",
+        "Review functional specifications",
+        "Identify design parameters",
+        "Assign development staff"
+    ];
+    $scope.collection = [
+        { id:1 , name: "Determine project scope", description: 'descript', text:'text' },
+        {id:2 , name: "Define preliminary resources"},
+        {    id:3 , name: "Conduct needs analysis"},
+        {   id:4 , name:  "Draft preliminary specifications"},
+        {   id:5 , name: "Develop delivery timeline"},
+        {   id:6 , name:  "Obtain approvals to proceed"},
+        {   id:7 , name: "Secure reuired resources"},
+        {   id:8 , name: "Develop functional specifications"},
+        {   id:9 , name: "Develop prototype"},
+        {           id:10 , name: "Review functional specifications"},
+        {   id:11 , name: "Identify design parameters"},
+        {   id:12 , name: "Assign development staff"}
+    ];
+
+    console.log ( 'Orgs initialised',$scope);
+}]);
+
+angular.module('phonegapFunApp')
+    .controller( 'ListCtrl', [ '$scope', function (scope ) {
+        scope.clicker = function ( what ) {
+            console.log(what, scope);
+            scope.$state.go('home');
+    }
+}] );
